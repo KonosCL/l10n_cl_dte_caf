f44c70b575162f83cddca6ee9f882e8ec41600f1
Nelson Ramírez Sánchez <info@nelsonramirez.com>
2017-11-04 12:41:40 -0300

    New Branch Commit

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'author': u'Blanco Martín & Asociados',
+    'category': 'Localization/Chile',
+    'depends': ['l10n_cl_invoice'],
+    "external_dependencies": {
+        'python': [
+            'xmltodict',
+            'base64'
+        ]
+    },
+    'description': u'''\n\nDTE CAF File Data Model\n\n''',
+    'installable': True,
+    'license': 'AGPL-3',
+    'name': 'CAF Container for DTE Compliance',
+    'test': [],
+    'data': [
+        'views/dte_caf.xml',
+        'security/ir.model.access.csv',
+    ],
+    'update_xml': [],
+    'version': '9.0.6.7',
+    'website': 'http://blancomartin.cl',
+    'auto-install': False,
+    'active': False
+}
